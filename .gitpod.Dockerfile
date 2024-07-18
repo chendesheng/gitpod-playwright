a2dbf6dc33d2a81c1c1fee6553a794aa8644a4d4
chendesheng <ichendesheng@gmail.com>
2024-07-18 01:12:13 +0000

    add visual regression test

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -2,3 +2,4 @@ FROM gitpod/workspace-full-vnc
 RUN pip install pytest-playwright
 RUN playwright install-deps
 RUN playwright install
+RUN pip install visual_regression_tracker
--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import BrowserContext
 
 import pytest
 @pytest.fixture(scope="session")
 def browser_context_args(request):
     return {"bypass_csp": True}
+
+@pytest.fixture()
+def control_panel_page(context: BrowserContext):
+    page = context.new_page()
+    page.goto('https://livechat6dash.testing.comm100dev.io/login')
+    return page
