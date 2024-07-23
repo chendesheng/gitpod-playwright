8c89f47f2dbebe8712602eb24e4887506794db42
chendesheng <ichendesheng@gmail.com>
2024-07-23 09:47:46 +0000

    use pytest-playwright-visual instead

--- test_visual.py
+++ test_visual.py
@@ -1,34 +1,9 @@
 import pytest
 import re
 from playwright.sync_api import Page, Browser, BrowserContext, expect, TimeoutError
-from playwright.sync_api import sync_playwright
+import pytest_playwright_visual
 
-from visual_regression_tracker import Config, IgnoreArea
-from visual_regression_tracker.playwright import PlaywrightVisualRegressionTracker, PageTrackOptions, \
-    PageScreenshotOptions, Agent, ElementHandleScreenshotOptions, ElementHandleTrackOptions
+def test_login_ui(browser: Browser, control_panel_page: Page, assert_snapshot):
+  control_panel_page.wait_for_timeout(2*1000)
+  assert_snapshot(control_panel_page.screenshot())
 
-def test_login_ui(browser: Browser, control_panel_page: Page):
-  control_panel_page.wait_for_timeout(10*1000)
-
-  vrt = PlaywrightVisualRegressionTracker(browser.browser_type, None)
-
-  with vrt:
-      vrt.trackPage(control_panel_page, 'Home page', PageTrackOptions(
-          diffTollerancePercent=1.34,
-          ignoreAreas=[
-              IgnoreArea(
-                  x=100,
-                  y=200,
-                  width=300,
-                  height=400,
-              )
-          ],
-          screenshotOptions=PageScreenshotOptions(
-              full_page=True,
-              omit_background=True,
-          ),
-          agent=Agent(
-              os='OS',
-              device='Device',
-          )
-      ))
